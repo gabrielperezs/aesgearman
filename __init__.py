c1071899a25d8b59ebee4f4618ddb76f26f7d926
Gabriel Pérez S <gabriel@gabriel-work.(none)>
2013-03-08 12:00:34 +0100

    contribution of @aaloy

--- __init__.py
+++ __init__.py
@@ -1,52 +1,76 @@
+#!/usr/bin/env python
+# encoding: UTF-8
+# ----------------------------------------------------------------------------
+
+"""
+Python class metod to use JSON + AES
+
+Modify this file to include AESKYEY
+
+"""
+
 import gearman
-import json
 from Crypto import Random
 from Crypto.Cipher import AES
 import json
 import base64
-import hashlib
+import logging
 
-
-AESKEY = ''
+log = logging.getLevelName(__name__)
 
 class AESCipher:
-        def __init__( self, key ):
-                self.BS = 16
-                self.pad = lambda s: s + (self.BS - len(s) % self.BS) * chr(self.BS - len(s) % self.BS)
-                self.unpad = lambda s : s[0:-ord(s[-1])]
-                self.key = key[0:16]
+    """
+    AESCipher
+    """
 
-        def encrypt( self, raw ):
-                raw = self.pad( json.dumps(raw) )
-                iv = Random.new().read( AES.block_size )
-                cipher = AES.new( self.key, AES.MODE_CBC, iv )
-                return base64.b64encode( iv + cipher.encrypt( raw ) )
+    def __init__(self, key):
+        if len(key) < 16:
+            raise ValueError(u"Key must be al lest 16 chars long")
+        self.BS = 16
+        self.key = key[:16]
 
-        def decrypt( self, enc ):
-		try:
-			enc = base64.b64decode(enc)
-			iv = enc[:16]
-			cipher = AES.new(self.key, AES.MODE_CBC, iv )
-			return json.loads( self.unpad(cipher.decrypt( enc[16:] )) )
-		except:
-			return False
-			raise NameError('Hola')
+    def pad(self, s):
+        return s + (self.BS - len(s) % self.BS) *\
+            chr(self.BS - len(s) % self.BS)
+
+    def unpad(self, s):
+        return s[0:-ord(s[-1])]
+
+    def encrypt(self, raw):
+        raw = self.pad(json.dumps(raw))
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw))
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:16]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return json.loads(self.unpad(cipher.decrypt(enc[16:])))
 
 
 class AESDataEncoder(gearman.DataEncoder):
-        @classmethod
-        def encode(cls, encodable_object):
-                return AESCipher( AESKEY ).encrypt( encodable_object )
+    @classmethod
+    def encode(cls, encodable_object):
+        log.debug('AES Encoder')
+        return AESCipher(AESKEY).encrypt(encodable_object)
 
-        @classmethod
-        def decode(cls, decodable_string):
-                return AESCipher( AESKEY ).decrypt(decodable_string)
+    @classmethod
+    def decode(cls, decodable_string):
+        log.debug('AES encoder')
+        return AESCipher(AESKEY).decrypt(decodable_string)
 
 
 class AESJSON_GearmanClient(gearman.GearmanClient):
-        data_encoder = AESDataEncoder
+    data_encoder = AESDataEncoder
+
 
 class AESJSON_GearmanWorker(gearman.GearmanWorker):
-        data_encoder = AESDataEncoder
+    data_encoder = AESDataEncoder
 
-
+if __name__ == '__main__':
+    cipher = AESCipher(key='12345678790123456')
+    s = cipher.encrypt('hello world')
+    print s
+    d = cipher.decrypt(s)
+    print d
